f8dcd723fb1b6ebdf715d3344d05302940a7685f
Tim Nhan Ta <TimTa@MacBook-Pro.local>
2017-12-12 15:47:36 +0100

    [TASK] change to typo3 v8 compatible

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '8.7.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
