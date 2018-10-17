134273acca3e186cfd37754ae75135e33b90ed4c
TimTa <timta@Resultifys-MacBook-Pro-2.local>
2018-10-17 17:06:38 +0200

    [TASK] add tca to only fetch category on the current pid of site

--- Configuration/TCA/Overrides/tx_pxaitemlist_domain_model_item.php
+++ Configuration/TCA/Overrides/tx_pxaitemlist_domain_model_item.php
@@ -5,7 +5,16 @@ call_user_func(
     function () {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
             'pxa_item_list',
-            'tx_pxaitemlist_domain_model_item'
+            'tx_pxaitemlist_domain_model_item',
+            // optional: in case the field would need a different name as "categories".
+            // The field is mandatory for TCEmain to work internally.
+            'categories',
+            // optional: add TCA options which controls how the field is displayed. e.g position and name of the category tree.
+            [
+                'fieldConfiguration' => [
+                    'foreign_table_where' =>  'AND sys_category.pid=###CURRENT_PID### AND sys_category.sys_language_uid IN (-1, 0)'
+                ]
+            ]
         );
     }
 );
